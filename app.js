88e2b26ade0d9647363283b455cd87ef23d3ec48
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-12 23:01:30 +0500

    initilise project by downloadingessential files

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const sequelize = require("./config/db");
+
+
+
+async function init() {
+  try {
+    await sequelize.authenticate();
+    console.log( "Database connected");
+
+    await sequelize.sync(); // ❗ no force:true
+    console.log(" Tables synced");
+  } catch (error) {
+    console.error(" Startup failed:", error.message);
+    process.exit(1);
+  }
+}
+
+init();
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});

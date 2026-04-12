ca03436a22ad4a6e7d55914713fb9a2be9568732
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-12 20:59:04 +0500

    Build search Model

---
+++ models/searchHistory.js
@@ -0,0 +1,7 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('SearchHistory', {
+    id:      { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    user_id: { type: DataTypes.UUID, allowNull: false },
+    query:   { type: DataTypes.STRING, allowNull: false },
+  });
+};

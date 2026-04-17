3364abc767bb51fb4db35259124be62c7daad10a
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-17 15:00:49 +0500

    BUILD update collection contoller

--- controllers/collectionsController.js
+++ controllers/collectionsController.js
@@ -27,7 +27,7 @@ const createCollection = async (req, res) => {
     res.status(201).json({
         sucess: true,
         message:"Collection created sucessfully",
-        data: collection
+        // data: collection
     })
 } catch (error) {
     console.error("❌ Create collection error:",error)
@@ -38,6 +38,65 @@ const createCollection = async (req, res) => {
     })
 }
 }
+// put collection 
+const updateCollection = async (req,res) => {
+    try {
+        const{ id } = req.params;
+        const { name,description } = req.body
+     const userId = req.user.id;
+        if (!id) {
+            return res.status(400).json({
+                sucess: false,
+                message: "id should not be empty"
+            })
+        }
+
+        const collection = await Collection.findOne({
+            where:{
+                id,
+                user_id: userId
+            }
+        });
+
+        if (!collection) {
+            return res.status(404).json({
+                sucess: false,
+                message: "Collection jot found"
+            })
+        }
+       const isEmpty = (val) =>
+    val === undefined ||
+    val === null ||
+    (typeof val === "string" && val.trim() === "");
+
+if (isEmpty(name) && isEmpty(description)) {
+    return res.status(200).json({
+        success: true,
+        message: "No changes provided, collection remains same",
+    });
+}
+        
+        if(name !== undefined) collection.name = name;
+        if(description !== undefined) collection.description = description;
+
+        await collection.save();
+
+        return res.status(200).json({
+            sucess: true,
+            message: "collection updated sucessfully"
+        })
+    }catch(error){
+          console.error("🔥 Update Collection Error:", error);
+
+        res.status(500).json({
+            sucess:false,
+            message: "internal server error"
+        })
+    }
+}
+
+
 module.exports = {
-    createCollection
+    createCollection,
+    updateCollection
 }

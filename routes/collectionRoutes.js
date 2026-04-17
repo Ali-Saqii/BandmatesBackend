c8433b6dae8c8e2a1097dd91a1e46c3cfef431cb
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-17 15:46:37 +0500

    import delecollection and getUserCollection in collection routes

--- routes/collectionRoutes.js
+++ routes/collectionRoutes.js
@@ -6,4 +6,6 @@ cControllers = require("../controllers/collectionsController")
 
 router.post("/create/collection",auth, cControllers.createCollection)
 router.put("/update/collection/:id",auth,cControllers.updateCollection)
+router.get("/get/collection",auth,cControllers.getUserCollection)
+router.delete("/delete/collection/:id",auth,cControllers.deleteCollection)
 module.exports = router

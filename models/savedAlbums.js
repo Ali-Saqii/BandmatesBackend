585c3314c3fc20b5c73c51a010a31c4e4d6b6a4d
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-17 19:38:30 +0500

    minor bug fix

--- models/savedAlbums.js
+++ models/savedAlbums.js
@@ -4,7 +4,7 @@ const savedAlbum = sequelize.define('SavedAlbum', {
   id:            { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
   user_id:       { type: DataTypes.UUID, allowNull: false },
   album_id:      { type: DataTypes.UUID, allowNull: false },
-  collection_id: { type: DataTypes.UUID, defaultValue: false },
+  collection_id: { type: DataTypes.UUID, allowNull: false },
 });
 
 module.exports = savedAlbum

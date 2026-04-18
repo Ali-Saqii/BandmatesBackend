1e2db8beb63a089d92cb014a89297989ff038e2c
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-18 15:48:50 +0500

    import controller in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,15 @@ const multer   = require("multer");
 const { signup, login }                       = require("../controllers/authcontroller");
 const { signupSchema, loginSchema }           = require("../validations/authValidation");
 const validate                                = require("../middleware/validate");
+const auth = require("../middleware/userAuth")
+const userController = require("../controllers/userControllers")
+
+
 
 const upload = multer({ dest: "uploads/avatars/" });
 router.post("/signup", upload.single("avatar"), validate(signupSchema), signup);
 router.post("/login", validate(loginSchema), login);
-
+router.get("/profile",auth,userController.getUserProFile)
+router.put("/update",auth,userController.updateUser)
+router.delete("/delete",auth,userController.deleteUser)
 module.exports = router;

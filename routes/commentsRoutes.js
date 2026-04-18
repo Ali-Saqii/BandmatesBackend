096e2d950feed5d9e7b9adf9f55e0a324736b4c3
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-18 22:48:01 +0500

    clean

--- routes/commentsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express  = require('express');
-const router   = express.Router();
-const auth     = require('../middleware/userAuth');
-const { postComment } = require('../controllers/commentController');
-
-
-router.post('/postComment/:album_id', auth , postComment);
-
-module.exports = router;

c34023a867775ecbdc65dd83a527f4ef098a3e29
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-19 19:27:19 +0500

    bug fix

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -2,8 +2,9 @@ const express  = require('express');
 const router   = express.Router();
 const auth     = require('../middleware/userAuth');
 const { postReview }= require('../controllers/reviewController');
+const { validateBody, postReviewSchema } = require('../validations/postReviewValidation'); // your validation file path
 
 
-router.post('/postReview/:album_id', auth, postReview);
+router.post('/postReview/:album_id', auth, validateBody(postReviewSchema),postReview);
 
 module.exports = router;

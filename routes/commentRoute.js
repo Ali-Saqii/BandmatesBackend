a8ae4ab0bc38851d6fa18955bc42b26562fea67e
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-19 19:27:07 +0500

    bug fix

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -2,8 +2,9 @@ const express  = require('express');
 const router   = express.Router();
 const auth     = require('../middleware/userAuth');
 const { postComment } = require('../controllers/commentController');
+const { validateBody, postCommentSchema } = require('../validations/commentvalidation'); // your validation file path
 
 
-router.post('/postComment/:album_id', auth, postComment);
+router.post('/postComment/:album_id', auth,validateBody(postCommentSchema), postComment);
 
 module.exports = router;

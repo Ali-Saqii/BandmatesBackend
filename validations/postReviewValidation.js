55b4cfb824e6ee3befb68a06b4b4319f8c094355
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-19 19:29:22 +0500

    tyutyuty

--- validations/postReviewValidation.js
+++ validations/postReviewValidation.js
@@ -28,7 +28,7 @@ const postReviewSchema = Joi.object({
 
 const validateBody = (schema) => (req, res, next) => {
   const { error, value } = schema.validate(req.body, { abortEarly: false });
-
+  
   if (error) {
     const errors = {};
     error.details.forEach(err => {

bba9670dea558174188d063deb2816393e0603cd
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-24 00:22:23 +0500

    import controller

--- routes/savedAlbumRoutes.js
+++ routes/savedAlbumRoutes.js
@@ -6,4 +6,5 @@ const sAcontrollers = require("../controllers/savedAlbumsController")
 
 router.post("/saveAlbum/add",auth,sAcontrollers.savedAlbum)
 router.delete("/savedAlbum/delete/:album_id/:collection_id",auth,sAcontrollers.removeAlbum)
+router.get("/user/savedAlbums/:id",auth, sAcontrollers.getUserSavedAlbums);
 module.exports = router

74fed5223f6d2a96ae35580b79595693cc555824
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-26 21:33:48 +0500

    updation

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 console.log('Last.fm API Key:', process.env.LASTFM_API_KEY);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 const subscriptionRoutes = require("./routes/subscriptionRoutes");
 app.use("/api/subscription", subscriptionRoutes);
